88b089c02eacf9ce03ca31275be04d3bdb2090fd
dgorski <d.gorski@fibargroup.com>
2020-06-30 15:18:43 +0200

    Updates

---
+++ precache-manifest.679f732d6a7f4c93260118ef84edc0cd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96d50198babb7dbe8ed8faab494ed5b",
+    "url": "/firstSite/index.html"
+  },
+  {
+    "revision": "fabfb07d3e9d7e085d1a",
+    "url": "/firstSite/static/css/main.3c987194.chunk.css"
+  },
+  {
+    "revision": "c224e112cf78dc861e4a",
+    "url": "/firstSite/static/js/2.316137f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/firstSite/static/js/2.316137f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fabfb07d3e9d7e085d1a",
+    "url": "/firstSite/static/js/main.397ed393.chunk.js"
+  },
+  {
+    "revision": "b369c2a95ce980838b45",
+    "url": "/firstSite/static/js/runtime-main.84adf3a8.js"
+  },
+  {
+    "revision": "c855703ed912aecbd2e2cdae89a8adfa",
+    "url": "/firstSite/static/media/img1.c855703e.jpg"
+  },
+  {
+    "revision": "b1dfb3a8be606646edbca4f1d9e05b67",
+    "url": "/firstSite/static/media/img2.b1dfb3a8.jpg"
+  },
+  {
+    "revision": "8aa6fbde6f801a8af63b8b6b31d38213",
+    "url": "/firstSite/static/media/img3.8aa6fbde.jpg"
+  },
+  {
+    "revision": "cdebf8cc9035ef98ce26cae677101003",
+    "url": "/firstSite/static/media/img4.cdebf8cc.jpg"
+  }
+]);
